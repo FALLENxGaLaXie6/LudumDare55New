86c52cc1ab081c4693fd548d4a3e0e516c4b5091
MERCUTIO\Mercutio <manytempers@gmail.com>
2024-04-14 14:09:56 -0700

    player character updates
    updated all player character sprites, added "walk thinking" animation

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,11 +3,11 @@ using System;
 
 public partial class Main : Node2D
 {
-    [Export] private PlayerMovement _playerMovement;
-    [Export] private Node2D _masterSceneNode;
+	[Export] private PlayerMovement _playerMovement;
+	[Export] private Node2D _masterSceneNode;
 
-    public override void _Ready()
-    {
-        _playerMovement.ReassignBubbleParent(_masterSceneNode);
-    }
+	public override void _Ready()
+	{
+		_playerMovement.ReassignBubbleParent(_masterSceneNode);
+	}
 }
--- Scripts/Player/PlayerAnimation.cs
+++ Scripts/Player/PlayerAnimation.cs
@@ -4,10 +4,10 @@ using System;
 public partial class PlayerAnimation : Node
 {
 	[Export] public AnimationPlayer _animationPlayer { get; private set; }
-    [Export] private float _idleAnimationSpeed = 1f;
-    [Export] private float _walkAnimationSpeed = 2f;
-    [Export] private float _jumpAnimationSpeed = 1f;
+	[Export] private float _idleAnimationSpeed = 1f;
+	[Export] private float _walkAnimationSpeed = 2f;
+	[Export] private float _jumpAnimationSpeed = 1f;
 
 
-    public void PlayAnimation(StringName animation, float speed = 1f) => _animationPlayer.Play(animation, -1D, speed);
+	public void PlayAnimation(StringName animation, float speed = 1f) => _animationPlayer.Play(animation, -1D, speed);
 }
